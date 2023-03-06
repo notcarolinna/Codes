39170dcebac1898317ad0c290620a796a5238505
Carolina Nóbrega <notcarolinna@Gmail.com>
2023-03-06 09:35:26 -0300

    Update tb.vhd

--- VHDL/Somador 4 bits/tb.vhd
+++ VHDL/Somador 4 bits/tb.vhd
@@ -7,33 +7,6 @@
     --------------------------------------
     -- Entidade
     --------------------------------------
-    ENTITY full_adder IS
-        PORT (
-            a, b, cin : IN STD_LOGIC;
-            cout, sum : OUT STD_LOGIC);
-    END ENTITY;
-
-    --------------------------------------
-    -- Arquitetura
-    --------------------------------------
-    ARCHITECTURE full_adder OF full_adder IS
-    BEGIN
-        cout <= (a AND b) OR ((a XOR b) AND cin);
-        sum <= a XOR b XOR cin;
-    END ARCHITECTURE;
-    Abaixo segue uma possível testbench para o código em VHDL do somador completo :
-
-    vhdl
-    Copy code
-    --------------------------------------
-    -- Biblioteca
-    --------------------------------------
-    LIBRARY IEEE;
-    USE IEEE.std_logic_1164.ALL;
-
-    --------------------------------------
-    -- Entidade
-    --------------------------------------
     ENTITY full_adder_tb IS
     END ENTITY;
 
