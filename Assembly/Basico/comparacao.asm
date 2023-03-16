3cdc318b2c759c8d4beb6a0fc41190a87f783b3b
Carolina Nóbrega <notcarolinna@Gmail.com>
2023-03-16 17:56:22 -0300

    Update comparacao.asm

--- Assembly/Basico/comparacao.asm
+++ Assembly/Basico/comparacao.asm
@@ -24,16 +24,16 @@ main:
 	li	$v0, 5					; o valor 5 é carregado no registrador $v0, que armazena o valor de retorno das chamadas de sistema	
 	syscall 					; chamada de sistema que executa uma operação específica
 
-	move	$t1, $v0			; copia o valor de um registrador para outro, ou seja, o valor de $v0 é copiado para $t1
+	move	$t1, $v0				; copia o valor de um registrador para outro, ou seja, o valor de $v0 é copiado para $t1
 	lw	$t0, i					; carrega uma palavra (4 bytes) da memória para o registrador
-	beq	$t0, $t1, v_Igual		; realiza um desvio condicional para o endereço especificado caso os dois registradores especificados como parâmetros forem iguais
+	beq	$t0, $t1, v_Igual			; realiza um desvio condicional para o endereço especificado caso os dois registradores especificados como parâmetros forem iguais
 		
 v_Diferente:
-	subi	$t1, $t1, 1			; subtrai uma constante de 16 bits do registrador: o valor 1 é substituído de $t1
+	subi	$t1, $t1, 1				; subtrai uma constante de 16 bits do registrador: o valor 1 é substituído de $t1
 	j	fim			
 
 v_Igual:
-	addi	$t0, $t0, 2			; adiciona o valor 2 ao registrador $t0
+	addi	$t0, $t0, 2				; adiciona o valor 2 ao registrador $t0
 
 fim:
 	li	$v0, 1				
