a4531c7415878e9253831d4dd553328e009042f9
Carolina Nóbrega <notcarolinna@Gmail.com>
2023-03-16 18:34:03 -0300

    Update PowerFunction.asm

--- Assembly/Macro Funções e Índice Ponteiros/PowerFunction.asm
+++ Assembly/Macro Funções e Índice Ponteiros/PowerFunction.asm
@@ -1,42 +1,47 @@
-;int power(int base, int expoente) {
+;	int power(int base, int expoente) {
 ;   int pow = 1;   
 ;   while(expoente >= 1) { // negativo n�o � tratado
-;     pow = pow * base;
-;     expoente--;
+;  	pow = pow * base;
+; 	expoente--;
 ;   }
 ;   return pow;
-; }
+; 	}
 
-; int a = 2, b = 3;
-; int res1, res2;
-; void main() {
-;   res1 = power(a, b);
-;   res2 = power(b, a);
-; }
+; 	int a = 2, b = 3;
+; 	int res1, res2;
+; 	void main() {
+; 	res1 = power(a, b);
+; 	res2 = power(b, a);
+; 	}
+
 .text
-  	.globl main		; |
-main:				; +-> main() {
+  	.globl main		
+
+main:				
 	lw $a0, a
 	lw $a1, b
-	jal power		; |
-	sw $v0, res1		; +-> ;   res1 = power(a, b);
+
+	jal power					; jump and link: chama a função power, salva o endereço de retorno na variável $ra e salta para o endereço de power
+	sw $v0, res1				; salva o resultado do primeiro cálculo na variável res1
 	lw $a0, b
 	lw $a1, a
-	jal power		; |
-	sw $v0, res2		; +-> ;   res2 = power(b, a);
-	li $v0, 10		; | $v0=10 encerra programa
-	syscall			; +-> chamada ao SO }
+	jal power					; chamada da função power, que salva o retorno em $ra
+	sw $v0, res2		
+	li $v0, 10		
+	syscall			
 
-power:				;int power(int base, int expoente) {
-	li $v0, 1		;   int pow = 1;  
+power:				
+	li $v0, 1	
+
 loop:
-	ble $a1, $zero, retorno ;   while(expoente >= 1) { // negativo n�o � tratado
-	mul $v0, $v0, $a0	;     pow = pow * base;
-	subi $a1, $a1, 1	;     expoente--;
-	j loop			;   }
-retorno:
-	jr $ra			;   return pow;
-				; }
+	ble $a1, $zero, retorno  	; ble: branch if less than or equal to, verifica se o expoente é menor ou igual a zero
+	mul $v0, $v0, $a0			; mul: multiply, multiplica a variável pow pela base em cada iteração do loop
+	subi $a1, $a1, 1	
+	j loop	
+
+retorno:						; jr: jump register, retorna da função power para a função que a chamou
+	jr $ra						; $ra: registrador usado para armazenar o endereço de retorno da função power		
+
 .data
 a: .word 2
 b: .word 3
