dcd315fa19e738a908e46998cc82e323cee40adf
Carolina Nóbrega <notcarolinna@Gmail.com>
2023-03-30 20:31:37 -0300

    Create HashTable.h

---
+++ C++/Hash/HashTable.h
@@ -0,0 +1,26 @@
+#ifndef HASHTABLE_H
+#define HASHTABLE_H
+
+#include "Node.h"
+#include <vector>
+
+class HashTable {
+
+private:
+	int capacidade_;
+	std::vector<Node*> tabela; // armazena os nodos da tabela hash
+
+	int hash(int key) const; // calcula o índice do vetor onde o nodo com a chave especificada deve ser inserido ou buscado
+
+public:
+	HashTable(int capacidade = 100);
+	~HashTable();
+
+	void insert(int key, int value);
+	bool remove(int key);
+	bool busca(int key, int& value) const;
+
+
+};
+
+#endif
